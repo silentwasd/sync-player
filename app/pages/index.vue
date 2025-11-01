525bbb09e89d4faa10153b4a00fb99c9c455e3de
BashkirskyKot <silentwasd@gmail.com>
2025-11-01 17:45:47 +0300

    Rooms

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {v4 as uuidv4} from 'uuid';
+
+async function createRoom() {
+    await navigateTo(`/rooms/${uuidv4()}`);
+}
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center w-dvw h-dvh">
+        <UButton label="Создать комнату" icon="i-mdi-plus" size="xl" @click="createRoom"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/rooms/[room].vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const sourceUrl      = ref<string>('');
+const videoPlayerRef = ref();
+
+function loadVideo() {
+    videoPlayerRef.value.load(sourceUrl.value);
+}
+</script>
+
+<template>
+    <UContainer class="py-5 flex flex-col gap-5">
+        <div>
+            <ClientOnly>
+                <VideoPlayer ref="videoPlayerRef" class="w-full aspect-[16/9] bg-black"/>
+
+                <template #fallback>
+                    <div class="w-full aspect-[16/9] bg-black flex items-center justify-center">
+                        <UIcon name="i-mdi-loading" class="size-24 text-white animate-spin"/>
+                    </div>
+                </template>
+            </ClientOnly>
+        </div>
+
+        <UForm @submit="loadVideo">
+            <UFormField label="URL" size="xl">
+                <UInput v-model="sourceUrl" class="w-full"/>
+            </UFormField>
+        </UForm>
+    </UContainer>
+</template>
+
+<style scoped>
+
+</style>
